804f15b44ddd6523ab2674ee00341e01f4ef8ac0
javieribaez17 <javieribaez7@gmail.com>
2025-01-27 21:50:48 -0500

    Descripción de los cambios, por ejemplo: Agregar nueva estructura de carpetas y actualizar index.html

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>© 2025 Mi Proyecto. Todos los derechos reservados.</p>
+</footer>
+<script src="/public/js/scripts.js"></script>
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a href="/public/index.php">Inicio</a></li>
+        <li><a href="/public/contacto.php">Contacto</a></li>
+    </ul>
+</nav>
